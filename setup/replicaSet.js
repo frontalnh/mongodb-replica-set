a8f4131e9926ff4fec1401e9081d543d9931fb95
namhoon <frontalnh@gmail.com>
2018-10-21 04:44:14 +0900

    init

---
+++ setup/replicaSet.js
@@ -0,0 +1,10 @@
+cfg = {
+  _id: 'rs0',
+  members: [
+    { _id: 0, host: 'mongo-rs0-1:27017' },
+    { _id: 1, host: 'mongo-rs0-2:27017' },
+    { _id: 2, host: 'mongo-rs0-3:27017' }
+  ]
+};
+cfg.protocolVersion = 1;
+rs.reconfig(cfg, { force: true });
---
+++ setup/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/sh
+echo ************************************
+echo Starting the replica Set
+echo ************************************
+
+sleep 10 | echo Sleeping
+
+mongo mongodb://mongo-rs0-1:27017 replicaSet.js
